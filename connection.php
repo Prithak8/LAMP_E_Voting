0ef0e67be873cd256181fede2994a9b5a9fa9557
Prithak Gajurel <109690999+Prithak8@users.noreply.github.com>
2024-03-11 12:50:54 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php 
-$con = mysqli_connect("localhost:3307","root","","secure_e_voting") or die ("error" . mysqli_error($con));
+$con = mysqli_connect("localhost:3308","root","","secure_e_voting") or die ("error" . mysqli_error($con));
 ?>
